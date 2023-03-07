e4a8fa20d683026892c8b619c02b7d41d0782578
Navendu0 <65621942+Navendu0@users.noreply.github.com>
2023-03-07 20:44:58 +0530

    remove whats apps mag

--- src/Screens/ProductDetails/ProductDetails.jsx
+++ src/Screens/ProductDetails/ProductDetails.jsx
@@ -151,7 +151,7 @@ function ProductDetails() {
 
           <button
 //this is the wwhatsapps number
-          onClick={()=>window.open('https://wa.me/919564144396/?text=Hi thare whatsapps j')}
+          onClick={()=>window.open('https://wa.me/919564144396/?text=Hi thare ')}
             style={{
               backgroundColor: "#44eb61",
               width: "100%",
