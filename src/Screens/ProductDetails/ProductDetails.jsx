cda858668116c84b6a6b13a91390d19b084fe85d
Navendu0 <65621942+Navendu0@users.noreply.github.com>
2023-03-07 20:31:13 +0530

    Update ProductDetails.jsx
    add ne w number

--- src/Screens/ProductDetails/ProductDetails.jsx
+++ src/Screens/ProductDetails/ProductDetails.jsx
@@ -151,7 +151,7 @@ function ProductDetails() {
 
           <button
 //this is the wwhatsapps number
-          onClick={()=>window.open('https://wa.me/919564144396/?text=Hi thare w')}
+          onClick={()=>window.open('https://wa.me/919564144396/?text=Hi thare wa')}
             style={{
               backgroundColor: "#44eb61",
               width: "100%",
